bb37c18d60f04c9ba8b0e02023b816817254bdca
FerG0373 <fer.dg.sc@gmail.com>
2025-05-13 23:13:15 -0300

    lblBienvenidos; btnEmpezar; frmGestion

---
+++ actividad-dos/catalog-manager-app/frmPrincipal.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace catalog_manager_app
+{
+    partial class frmPrincipal
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblBienvenidos = new System.Windows.Forms.Label();
+            this.btnEmpezar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblBienvenidos
+            // 
+            this.lblBienvenidos.AutoSize = true;
+            this.lblBienvenidos.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBienvenidos.Location = new System.Drawing.Point(315, 86);
+            this.lblBienvenidos.Name = "lblBienvenidos";
+            this.lblBienvenidos.Size = new System.Drawing.Size(191, 31);
+            this.lblBienvenidos.TabIndex = 0;
+            this.lblBienvenidos.Text = "¡Bienvenidos!";
+            // 
+            // btnEmpezar
+            // 
+            this.btnEmpezar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEmpezar.Location = new System.Drawing.Point(363, 239);
+            this.btnEmpezar.Name = "btnEmpezar";
+            this.btnEmpezar.Size = new System.Drawing.Size(95, 34);
+            this.btnEmpezar.TabIndex = 1;
+            this.btnEmpezar.Text = "Empezar";
+            this.btnEmpezar.UseVisualStyleBackColor = true;
+            this.btnEmpezar.Click += new System.EventHandler(this.btnEmpezar_Click);
+            // 
+            // frmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnEmpezar);
+            this.Controls.Add(this.lblBienvenidos);
+            this.Name = "frmPrincipal";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Home";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblBienvenidos;
+        private System.Windows.Forms.Button btnEmpezar;
+    }
+}
+
