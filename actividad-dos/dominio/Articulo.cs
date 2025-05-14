6f4a5d2a83faa7b55b3914d196c7aaa53e25f599
FerG0373 <fer.dg.sc@gmail.com>
2025-05-13 22:17:24 -0300

    Solución, Clases, Referencias

---
+++ actividad-dos/dominio/Articulo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace dominio
+{
+    public class Articulo
+    {
+        public int CodArticulo { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public Marca Marca { get; set; }
+        public Categoria Categoria { get; set; }
+        public Imagen Imagen { get; set; }
+        public double Precio { get; set; }
+    }
+}
